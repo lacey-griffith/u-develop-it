10c57ca53788dd12f8a4863ef4712129491a832f
Lacey <lacey.griffith@g.austincc.edu>
2021-04-27 11:28:02 -0500

    add routes, test for successful connection to server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//default response for requests not found must be last get route! 
+app.use((req,res) => {
+    res.status(404).end();
+})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
