1ab0f0e9b2a472d7410640aaed4b6478ecb481cf
Lacey <lacey.griffith@g.austincc.edu>
2021-04-27 12:18:23 -0500

    add sql query

--- server.js
+++ server.js
@@ -3,24 +3,29 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+const mysql = require('mysql2');
+
+//secure information
+require('dotenv').config();
+let pw = process.env.PW;
+let database = process.env.DATABASE;
+
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: `${pw}`,
+      database: `${database}`
+    },
+    console.log(`Connected to the ${database} database.`)
+  );
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 //default response for requests not found must be last get route! 
 app.use((req,res) => {
     res.status(404).end();
