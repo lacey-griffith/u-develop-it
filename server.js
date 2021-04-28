6ba34edf3d9afe0ce7813abeda1f63c2cf58de64
Lacey <lacey.griffith@g.austincc.edu>
2021-04-28 10:10:18 -0500

    update code for assistance on post req

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
 const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //secure information
 require('dotenv').config();
@@ -13,47 +16,47 @@ let database = process.env.DATABASE;
 
 
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: `${pw}`,
-      database: `${database}`
-    },
-    console.log(`Connected to the ${database} database.`)
-  );
-
+  {
+    host: 'localhost',
+    user: 'root',
+    password: `${pw}`,
+    database: `${database}`
+  },
+  console.log(`Connected to the ${database} database.`)
+);
 
 // get all candidates
 app.get('/api/candidates', (req, res) => {
   const sql = `SELECT * FROM candidates`;
 
   db.query(sql, (err, rows) => {
-    if(err){
-      res.status(500).json({error: err.message})
+    if (err) {
+      res.status(500).json({ error: err.message });
       return;
     }
     res.json({
       message: 'success',
       data: rows
-    })
-  })
-})
+    });
+  });
+});
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE ID = ?`;
-  const params = [req.params.id]
-db.query(sql, params, (err, row) => {
-  if (err) {
-    res.status(400).json({error: err.message});
-    return;
-  }
-  res.json({
-    message: 'success',
-    data: row
-  })
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
-})
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
@@ -62,40 +65,41 @@ app.delete('/api/candidate/:id', (req, res) => {
 
   db.query(sql, params, (err, result) => {
     if (err) {
-      res.statusMessage(400).json({
-        error: res.message
-      })
+      res.statusMessage(400).json({ error: res.message });
     } else if (!result.affectedRows) {
       res.json({
-        message: 'Candidate not found.'
-      })
+        message: 'Candidate not found'
+      });
     } else {
       res.json({
         message: 'deleted',
         changes: result.affectedRows,
         id: req.params.id
-      })
+      });
     }
-  })
-})
+  });
+});
 
 // Create a candidate
-app.post('/api/candidate', ({body}, res) => {
-  const errors = inputCheck(body,'first_name', 'last_name', 'industry_connected');
-  console.log(body.first_name)
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
   if (errors) {
-    res.status(400).json({error: errors});
+    res.status(400).json({ error: errors });
     return;
   }
+
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  VALUES (?,?,?)`;
+    VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
 
   db.query(sql, params, (err, result) => {
     if (err) {
-      res.status(400).json({
-        error: err.message
-      });
+      res.status(400).json({ error: err.message });
       return;
     }
     res.json({
@@ -103,12 +107,13 @@ app.post('/api/candidate', ({body}, res) => {
       data: body
     });
   });
-})
+});
 
 //default response for requests not found must be last get route! 
-app.use((req,res) => {
-    res.status(404).end();
-})
+app.use((req, res) => {
+  res.status(404).end();
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
