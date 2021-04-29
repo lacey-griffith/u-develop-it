03797355f650323db37719bc99ba83d2707cad1b
Lacey <lacey.griffith@g.austincc.edu>
2021-04-29 07:30:14 -0500

    update candidate party, delete a party

--- server.js
+++ server.js
@@ -25,6 +25,62 @@ const db = mysql.createConnection(
   console.log(`Connected to the ${database} database.`)
 );
 
+//get all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+
+  db.query(sql, (err, rows) => {
+    if(err){
+      res.status(500).json({error: err.message});
+      return
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    })
+  })
+})
+
+//get party by id
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if(err){
+      res.status(500).json({error: err.message});
+      return
+    }
+    res.json({
+      message: 'success',
+      data: row
+    })
+  })
+})
+
+// delete a party
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({error: res.message});
+      return
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Party not found'
+      })
+    } else {
+      res.json({
+        message: 'Party successfully deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
 // get all candidates
 app.get('/api/candidates', (req, res) => {
   const sql = `SELECT candidates.*, parties.name 
@@ -117,6 +173,34 @@ app.post('/api/candidate', ({body}, res) => {
     });
   });
 });
+
+//update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+
+if (errors) {
+  res.status(400).json({ error: errors });
+  return;
+}
+  const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if(err){
+      res.status(400).json({error: err.message});
+    }
+    else if(!result.affectedRows){
+      res.json({message: 'Candidate not found'});
+    }
+    else {
+      res.json({
+        message: 'Update success!',
+        data: req.body,
+        changes: result.affectedRows
+      })
+    }
+  })
+})
 
 //default response for requests not found must be last get route! 
 app.use((req, res) => {
